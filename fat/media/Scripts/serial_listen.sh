7f0cec82bd1d7a9ce0db4beb92fbedb8bf5899c2
illusion-pasture-program <96557327+illusion-pasture-program@users.noreply.github.com>
2021-12-22 22:05:00 -0500

    Update serial_listen.sh
    Cleaning commented-out old code

--- fat/media/Scripts/serial_listen.sh
+++ fat/media/Scripts/serial_listen.sh
@@ -2,10 +2,5 @@
 
 sleep 5
 export PATH="/media/fat/Scripts:$PATH"
-#stty 9600 -F /dev/ttyACM0 raw -echo
-#bash -v </dev/ttyACM0 
-#bash
 stty 9600 -F /dev/ttyUSB0 raw -echo
-#bash -v </dev/ttyUSB0 
 bash </dev/ttyUSB0
-#bash
