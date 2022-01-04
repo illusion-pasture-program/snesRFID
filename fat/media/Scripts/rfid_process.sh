d090744161f70d448c2a16e713d085f66d90f07c
illusion-pasture-program <96557327+illusion-pasture-program@users.noreply.github.com>
2022-01-04 11:50:42 -0500

    Update rfid_process.sh
    Adding a "enablesave" flag. When turned on, the rom will load using the visible menu (the load_rom method of mbc)

--- fat/media/Scripts/rfid_process.sh
+++ fat/media/Scripts/rfid_process.sh
@@ -6,18 +6,27 @@ qload_rom()
     then	
 	export LASTROM="$3"
 	echo "$LASTROM"
-	cp /media/fat/Games/"$1"/"$2" /media/fat/Games/"$1"/boot1.rom
-	mbc load_core /media/fat/_Console/"$1"*.rbf
+
+        if [ "$4" == "enablesave" ]
+        then
+        #echo "save enabled game"
+        rm /media/fat/Games/"$1"/boot1.rom
+        mbc load_rom "$1" /media/fat/Games/"$1"/"$2"
+        else
+        #echo "no save game"
+        cp /media/fat/Games/"$1"/"$2" /media/fat/Games/"$1"/boot1.rom
+        mbc load_core /media/fat/_Console/"$1"*.rbf
+        fi	
     fi    
 }
 
-case "$1" in
+case "$1" in   
    "noscan") qload_rom Atari2600 noscan.rom "$1";;   
    "4034012226") qload_rom NES Anticipation.nes "$1";;
    "3826459960") qload_rom NES BubbleBobble.nes "$1";;
-   "3978110274") qload_rom NES Castlevania.nes "$1";;   
+   "3978110274") qload_rom NES Castlevania.nes "$1" enablesave;;   
    "1826759746") qload_rom SNES Aladdin.sfc "$1";;   
-   "1107764546") qload_rom SNES DK1.sfc "$1";;
-   "2398168120") qload_rom SNES DK2.sfc "$1";;
-   "3921617986") qload_rom SNES DK3.sfc "$1";;   
+   "1107764546") qload_rom SNES DK1.sfc "$1" enablesave;;
+   "2398168120") qload_rom SNES DK2.sfc "$1" enablesave;;
+   "3921617986") qload_rom SNES DK3.sfc "$1" enablesave;;   
 esac
