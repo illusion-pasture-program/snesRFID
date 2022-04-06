eae1218df3c176105b5c820fc0519df940a746ba
illusion-pasture-program <96557327+illusion-pasture-program@users.noreply.github.com>
2022-04-06 15:32:22 -0400

    Update rfid_process.sh
    Updating method of loading ROMs from MiSTer Batch Control to the new MiSTer Game Launcher files (MGL) method. Use must use a version of MiSTer from Feb 24, 2022 or after to support this method of launching. This will create an MGL file automatically if one has not already been created for the associated rom.

--- fat/media/Scripts/rfid_process.sh
+++ fat/media/Scripts/rfid_process.sh
@@ -7,26 +7,22 @@ qload_rom()
 	export LASTROM="$3"
 	echo "$LASTROM"
 
-        if [ "$4" == "enablesave" ]
-        then
-        #echo "save enabled game"
-        rm /media/fat/Games/"$1"/boot1.rom
-        mbc load_rom "$1" /media/fat/Games/"$1"/"$2"
-        else
-        #echo "no save game"
-        cp /media/fat/Games/"$1"/"$2" /media/fat/Games/"$1"/boot1.rom
-        mbc load_core /media/fat/_Console/"$1"*.rbf
-        fi	
+        if [ ! -f /media/fat/Games/"$1"/"$2".mgl ]
+            then                
+                echo "<mistergamedescription><rbf>_console/"$1"</rbf><file delay=\"2\" type=\"f\" index=\"0\" path=\"../../games/"$1"/"$2"\"/></mistergamedescription>" > "/media/fat/Games/"$1"/"$2".mgl"
+        fi
+        echo "load_core /media/fat/Games/"$1"/"$2".mgl" > /dev/MiSTer_cmd
+	
     fi    
 }
 
-case "$1" in   
+case "$1" in 
    "noscan") qload_rom Atari2600 noscan.rom "$1";;   
    "4034012226") qload_rom NES Anticipation.nes "$1";;
    "3826459960") qload_rom NES BubbleBobble.nes "$1";;
-   "3978110274") qload_rom NES Castlevania.nes "$1" enablesave;;   
+   "3978110274") qload_rom NES Castlevania.nes "$1";;   
    "1826759746") qload_rom SNES Aladdin.sfc "$1";;   
-   "1107764546") qload_rom SNES DK1.sfc "$1" enablesave;;
-   "2398168120") qload_rom SNES DK2.sfc "$1" enablesave;;
-   "3921617986") qload_rom SNES DK3.sfc "$1" enablesave;;   
+   "1107764546") qload_rom SNES DK1.sfc "$1";;
+   "2398168120") qload_rom SNES DK2.sfc "$1";;
+   "3921617986") qload_rom SNES DK3.sfc "$1";;   
 esac
