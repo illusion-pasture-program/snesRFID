ef671cc34951c4aecee7aa0dbda44f3f678942e1
illusion-pasture-program <96557327+illusion-pasture-program@users.noreply.github.com>
2022-05-04 21:00:17 -0400

    Update serial_listen.sh
    Updating loading method based on kconger/MiSTer_tty2epaper project. RFID reader now gets to "READY!" screen both faster and more reliably.

--- fat/media/Scripts/serial_listen.sh
+++ fat/media/Scripts/serial_listen.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sleep 5
-export PATH="/media/fat/Scripts:$PATH"
-stty 9600 -F /dev/ttyUSB0 raw -echo
-bash </dev/ttyUSB0
+WAITEND=$((SECONDS+30))
+while !  [ -c "/dev/ttyUSB0" ] && [ ${SECONDS} -lt ${WAITEND} ]; do	
+	sleep 0.1
+done
+
+if [ -c "/dev/ttyUSB0" ]; then 	
+	export PATH="/media/fat/Scripts:$PATH"
+	stty 9600 -F /dev/ttyUSB0 raw -echo
+	bash </dev/ttyUSB0
+fi
