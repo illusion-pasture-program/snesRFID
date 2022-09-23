1dbeacb7a0f62f6e0167f4af36127a57d7095e19
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2022-09-23 15:22:22 -0700

    Update serial_listen.sh

--- fat/media/Scripts/serial_listen.sh
+++ fat/media/Scripts/serial_listen.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+ttydev="/dev/ttyUSB0"
+
 WAITEND=$((SECONDS+30))
-while !  [ -c "/dev/ttyUSB0" ] && [ ${SECONDS} -lt ${WAITEND} ]; do	
+while !  [ -c "${ttydev}" ] && [ ${SECONDS} -lt ${WAITEND} ]; do	
 	sleep 0.1
 done
 
-if [ -c "/dev/ttyUSB0" ]; then 	
+if [ -c "${ttydev}" ]; then 	
 	export PATH="/media/fat/Scripts:$PATH"
-	stty 9600 -F /dev/ttyUSB0 raw -echo
-	bash </dev/ttyUSB0
+	stty 9600 -F "${ttydev}" raw -echo
+	bash <"${ttydev}"
 fi
